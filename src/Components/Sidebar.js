1900d83f4be1a37c8f768121e153000d5dfb6174
CYX <dangeboe@gmail.com>
2025-04-04 16:32:52 -0400

    Edit Comments

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,7 +2,6 @@ import React, { forwardRef } from "react";
 import { Menu } from "antd";
 import { RadarChartOutlined } from "@ant-design/icons";
 
-// 使用 forwardRef 来传递 ref 给 Menu 组件
 const Sidebar = forwardRef(({ defaultSelected }, ref) => {
   const items = [
     {
@@ -14,11 +13,11 @@ const Sidebar = forwardRef(({ defaultSelected }, ref) => {
 
   return (
     <Menu
-      ref={ref} // 这里传递 ref
+      ref={ref}
       mode="inline"
       defaultSelectedKeys={[defaultSelected]} // 默认选中资产探测
       style={{ height: "100%", borderRight: 0 }}
-      items={items} // 使用 items 替代 children
+      items={items}
     />
   );
 });
